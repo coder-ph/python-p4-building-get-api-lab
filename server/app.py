e84676fb83f7ac3f92f1502aac133757dca200a0
Phelix <phelixmbani@gmail.com>
2025-01-17 13:51:15 +0300

    build get API

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy import desc
 
 from models import db, Bakery, BakedGood
 
@@ -20,19 +21,33 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    response = make_response(jsonify(bakeries), 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    if bakery:
+        response = make_response(jsonify(bakery.to_dict()), 200)
+    else:
+        response = make_response({'error': f'bakery {id} not found'}, 404)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = [bg.to_dict() for bg in BakedGood.query.order_by(desc(BakedGood.price))]
+    response = make_response(jsonify(baked_goods), 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(desc(BakedGood.price)).first()
+    if most_expensive:
+        response = make_response(jsonify(most_expensive.to_dict()), 200)
+    else:
+        response = make_response({'error': 'No baked goods found'}, 404)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
